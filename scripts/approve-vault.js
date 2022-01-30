e70a2f8078157a79279b11b1f8b9fb95e5887434
Magnus Brantheim <m.brantheim@gmail.com>
2022-01-30 21:52:07 +0000

    Update approve script

--- scripts/approve-vault.js
+++ scripts/approve-vault.js
@@ -1,10 +1,10 @@
 async function main() {
-  const vaultAddress = '0xC2cE269f3646a5F5bF1cCDa73c6cAB50f64012b6';
-  const ERC20 = await ethers.getContractFactory('contracts/ERC20.sol:ERC20');
-  const fUSDTAddress = '0x049d68029688eabf473097a2fc38ef61633a3c7a';
-  const fUSDT = await ERC20.attach(fUSDTAddress);
-  await fUSDT.approve(vaultAddress, ethers.utils.parseEther('100'));
-  console.log('fUSDT approved');
+  const vaultAddress = "0x35B01DB4f903F42052ab8562BaBC38781aB01205";
+  const ERC20 = await ethers.getContractFactory("contracts/ERC20.sol:ERC20");
+  const wftmAddress = "0x21be370d5312f44cb42ce377bc9b8a0cef1a4c83";
+  const wftm = await ERC20.attach(wftmAddress);
+  await wftm.approve(vaultAddress, ethers.utils.parseEther("100"));
+  console.log("wftm approved");
 }
 
 main()
