b8b910dda1ab68847fe6b8388892605ffe61f415
Magnus Brantheim <m.brantheim@gmail.com>
2022-01-30 21:29:15 +0000

    Update deploy scripts

--- scripts/deploy/1-vault.js
+++ scripts/deploy/1-vault.js
@@ -1,22 +1,14 @@
 async function main() {
   const Vault = await ethers.getContractFactory("ReaperVaultv1_3");
 
-  //const usdcAddress = "0x04068da6c83afcfa0e13ba15a6696662335d5b75";
-  const fUSDTAddress = "0x049d68029688eabf473097a2fc38ef61633a3c7a";
-  const tokenName = "fUSDT SCREAM Single Sided";
-  const tokenSymbol = "rf-scfUSDT";
+  const wftmAddress = "0x21be370d5312f44cb42ce377bc9b8a0cef1a4c83";
+  const tokenName = "WFTM Iron Bank Single Sided";
+  const tokenSymbol = "rf-iWFTM";
   const approvalDelay = 0;
   const depositFee = 0;
-  const tvlCap = ethers.utils.parseEther("5000");
+  const tvlCap = ethers.utils.parseEther("1000");
 
-  const vault = await Vault.deploy(
-    fUSDTAddress,
-    tokenName,
-    tokenSymbol,
-    approvalDelay,
-    depositFee,
-    tvlCap
-  );
+  const vault = await Vault.deploy(wftmAddress, tokenName, tokenSymbol, approvalDelay, depositFee, tvlCap);
 
   await vault.deployed();
   console.log("Vault deployed to:", vault.address);
@@ -24,7 +16,7 @@ async function main() {
 
 main()
   .then(() => process.exit(0))
-  .catch((error) => {
+  .catch(error => {
     console.error(error);
     process.exit(1);
   });
--- scripts/deploy/3-initialize.js
+++ scripts/deploy/3-initialize.js
@@ -1,13 +1,12 @@
 async function main() {
-  const vaultAddress = '0xC2cE269f3646a5F5bF1cCDa73c6cAB50f64012b6';
-  const strategyAddress = '0xb2C1De1f7f03054BDCbeC35f9f8370C09719fF1D';
+  const vaultAddress = "0x35B01DB4f903F42052ab8562BaBC38781aB01205";
+  const strategyAddress = "0xB194Dc2AAbaCC11517296E2aba8037d5bB4982D1";
 
-  const Vault = await ethers.getContractFactory('ReaperVaultv1_3');
+  const Vault = await ethers.getContractFactory("ReaperVaultv1_3");
   const vault = Vault.attach(vaultAddress);
 
-  //const options = { gasPrice: 2000000000000, gasLimit: 9000000 };
   await vault.initialize(strategyAddress);
-  console.log('Vault initialized');
+  console.log("Vault initialized");
 }
 
 main()
